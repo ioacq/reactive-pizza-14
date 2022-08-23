e769ef345f117dd5724b4f2b45a270c2eb0b417c
ioacq <iocampo.acq@gmail.com>
2022-08-23 09:49:30 +0800

    1 file modified

--- src/app/pizza/pizza.service.ts
+++ src/app/pizza/pizza.service.ts
@@ -5,23 +5,6 @@ import { Pizza, Topping } from './pizza.interface';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
 
-export interface State {
-  pizzas: Pizza[],
-  toppings: Topping[]
-}
-
-const state: State = {
-  pizzas: [
-    { name: 'New Yorker', toppings: ['Bacon', 'Pepperoni', 'Ham', 'Mushrooms'] },
-    { name: 'Hot & Spicy', toppings: ['Jalapenos', 'Herbs', 'Pepperoni', 'Chicken'] },
-    { name: 'Hawaiian', toppings: ['Ham', 'Pineapple', 'Sweetcorn'] }
-  ],
-  toppings: [
-    'Bacon', 'Pepperoni', 'Mushrooms', 'Herbs',
-    'Chicken', 'Pineapple', 'Ham', 'Jalapenos'
-  ]
-};
-
 export const PIZZAS: Pizza[] = [
   { name: 'New Yorker', toppings: ['Bacon', 'Pepperoni', 'Ham', 'Mushrooms'] },
   { name: 'Hot & Spicy', toppings: ['Jalapenos', 'Herbs', 'Pepperoni', 'Chicken'] },
@@ -33,6 +16,16 @@ export const TOPPINGS: Topping[] = [
   'Bacon', 'Pepperoni', 'Mushrooms', 'Herbs',
   'Chicken', 'Pineapple', 'Ham', 'Jalapenos'
 ];
+
+export interface State {
+  pizzas: Pizza[],
+  toppings: Topping[]
+}
+
+const state: State = {
+  pizzas: PIZZAS,
+  toppings: TOPPINGS
+};
 
 @Injectable()
 export class PizzaService {
